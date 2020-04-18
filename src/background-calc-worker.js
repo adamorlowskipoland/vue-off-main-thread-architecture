6245e24d52dd6b31b19140665fde7b364d5cb385
Adam <adam.orlowski@rtbhouse.com>
2020-04-18 20:27:36 +0200

    working highly coupled loading

--- src/background-calc-worker.js
+++ src/background-calc-worker.js
@@ -4,7 +4,9 @@ addEventListener("message", (event) => {
   console.log('%c background-calc-worker.js, 17, event, , ', 'color: lightseagreen; font-weight:' +
     ' 700', event);
   if (event.data.method === "calculatePrimes") {
-    postMessage({ nums: calculatePrimes(...event.data.args) });
+    postMessage({ key: 'loading', value: true });
+    postMessage({ key: 'nums', value: calculatePrimes(...event.data.args) });
+    postMessage({ key: 'loading', value: false });
   } else {
     postMessage({ msg: "elo" });
   }
--- src/components/OffMainThread.vue
+++ src/components/OffMainThread.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>OffMainThread</h1>
-    <button class="btn" @click="generate">Generate</button>
+    <button class="btn" @click="generate">Generate {{loading}}</button>
     <generated-nums :nums="nums"/>
   </div>
 </template>
@@ -16,21 +16,27 @@
     components: { GeneratedNums },
     data() {
       return {
-        nums: []
+        nums: [],
+        loading: false,
       }
     },
     methods: {
       generate() {
-        this.$emit('loading', true)
+        // this.$emit('loading', true)
         bgCalc.postMessage({ method: 'calculatePrimes', args: [400, 1000000000] });
-        this.$emit('loading', false)
+        // this.$emit('loading', false)
       }
     },
     created() {
       bgCalc.onmessage = e => {
         console.log('%c OffMainThread.vue, , , , ', 'color: yellow; font-weight: 700',e);
-        this.nums = e.data.nums;
+        this[e.data.key] = e.data.value;
       };
+    },
+    watch: {
+      loading(update) {
+        this.$emit("loading", update)
+      }
     }
   }
 </script>
