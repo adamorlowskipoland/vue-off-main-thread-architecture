ba37f4141f43c9d3fcbd12daf1aa376a50f26feb
Adam <adam.orlowski@rtbhouse.com>
2020-04-20 21:44:21 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:adamorlowskipoland/vue-off-main-thread-architecture.git master:gh-pages
 
 cd -
